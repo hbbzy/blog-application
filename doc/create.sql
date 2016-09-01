93c9a5ece9ba18bbd7657a215ad97361d1353fe8
zhufudehada <zhufudehada@163.com>
2016-09-01 13:43:05 +0800

    增加文章分类

--- doc/create.sql
+++ doc/create.sql
@@ -26,6 +26,8 @@ CREATE TABLE IF NOT EXISTS `hf_manage`(
 CREATE TABLE IF NOT EXISTS `hf_blog`(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT
 	,`title` VARCHAR(180) NOT NULL DEFAULT '' COMMENT '标题'
+    ,`subtitle` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '副标题'
+	,`cid` SMALLINT UNSIGNED NOT NULL COMMENT '文章分类'
 	,`seotitle` VARCHAR(90) NOT NULL DEFAULT '' COMMENT 'SEO 标题'
 	,`seodesc` VARCHAR(512) NOT NULL DEFAULT '' COMMENT 'SEO 描述'
 	,`seokeywords` VARCHAR(130) NOT NULL DEFAULT '' COMMENT 'SEO 关键字'
@@ -37,18 +39,39 @@ CREATE TABLE IF NOT EXISTS `hf_blog`(
 	,`goods` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '点赞数'
 	,`status` enum('待审核','审核通过','拒绝','删除') DEFAULT '待审核' COMMENT '状态'
 	,`sort` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '排序'
+    ,`addtime` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '添加时间'
+    ,`updatetime` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '修改时间'
 	,PRIMARY KEY(`id`)
 	,KEY `authorid` (`authorid`)
-	,KEY `search` (`title`,`seokeywords`,`thumb`,`status`)
+	,KEY `search` (`title`,`seokeywords`,`thumb`,`status`,`updatetime`)
 )ENGINE=INNODB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT='博客表';
 
 #博客正文表
 CREATE TABLE IF NOT EXISTS `hf_blog_data`(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT
 	,`contents` text COMMENT 'pc端正文'
-	,`mcontents` text COMMENT '手机端正文'
 	,PRIMARY KEY `id` (`id`)
 )ENGINE=INNODB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT='博客正文表';
+
+
+#文章分类表
+CREATE TABLE IF NOT EXISTS `hf_category`(
+    `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT
+    ,`title` VARCHAR(180) NOT NULL DEFAULT '' COMMENT '分类名'
+    ,`catalog` VARCHAR(24) NOT NULL DEFAULT '' COMMENT '目录名 可定义'
+    ,`subtitle` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '副标题'
+    ,`seotitle` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'SEO 标题'
+    ,`seodesc` VARCHAR(512) NOT NULL DEFAULT '' COMMENT 'SEO 介绍'
+    ,`seokeywords` VARCHAR(198) NOT NULL DEFAULT '' COMMENT 'SEO 关键字'
+    ,`letter` char(1) NOT NULL DEFAULT '' COMMENT '首字母'
+    ,`pid` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '父id'
+    ,`pids` VARCHAR(1000) NOT NULL DEFAULT '' COMMENT '父id组'
+    ,`cids` VARCHAR(1000) NOT NULL DEFAULT '' COMMENT '子id组'
+    ,PRIMARY KEY(`id`)
+    ,UNIQUE KEY(`catalog`)
+    ,KEY `ids` (`id`,`pid`)
+    ,KEY `keys` (`letter`,`catalog`)
+)ENGINE=INNODB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT='分类表';
 
 CREATE TABLE IF NOT EXISTS `hf_webconfig`(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT
